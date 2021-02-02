9c6876e85a30ccbdad977c4a627c976d1f8a7415
Mike Richter <mrichter@preemptive.com>
2021-02-02 11:28:58 -0500

    Initial Commit

---
+++ PlanningPoker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlanningPoker
+//
+//  Created by Mike Richter on 2/2/21.
+//  Copyright © 2021 PreEmptive Solutions, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
